a98fb1757cb453bdb1e28290c362781c2cee58c7
luisrequejinho <luisrequejinho@luiss-air.wireless-1x.unc.edu>
2019-08-29 13:33:23 -0400

    trying to print initial and names

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,18 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		for (int n=0; n<3; n++) {
+ String Fname = scan.next();
+char Finitial = Fname.charAt(0);
+String Lname = scan.next();
+double sum = scan.nextDouble()
+ 
+		
+	
+System.out.println(Finitial);
+System.out.println(Lname);
+
+n++;
 	}
 }
